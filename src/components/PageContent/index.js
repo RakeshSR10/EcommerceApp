02e27819c8169bfd8b73c0ef2a152af8f25dcf2a
RakeshSR10 <rakeshsrking@gmail.com>
2023-06-15 12:06:37 +0530

    Developing ecom app using react and antD

---
+++ src/components/PageContent/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppRoutes from '../Routes';
+
+const PageContent = () => {
+    return (
+        <div className='appContent'>
+            <AppRoutes />
+        </div>
+    )
+}
+
+export default PageContent;
